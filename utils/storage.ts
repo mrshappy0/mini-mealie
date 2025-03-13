974a90f80379524e25e67a097c9e7dd4e26a53b8
mrshappy0 <ajs11300@gmail.com>
2025-03-13 15:15:11 -0600

    linting various files

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,20 +1,20 @@
-import { showBadge, clearBadge } from "./badge";
-import { addContextMenu, removeContextMenu } from "./contextMenu";
+import { showBadge, clearBadge } from './badge';
+import { addContextMenu, removeContextMenu } from './contextMenu';
 
 export const checkStorageAndUpdateBadge = () => {
     chrome.storage.sync.get(
-        ["mealieServer", "mealieApiToken"],
+        ['mealieServer', 'mealieApiToken'],
         ({ mealieServer, mealieApiToken }) => {
             const hasServer = !!mealieServer;
             const hasToken = !!mealieApiToken;
 
             if (!hasServer || !hasToken) {
-                showBadge("❌");
+                showBadge('❌');
                 removeContextMenu();
             } else {
                 clearBadge();
                 addContextMenu();
             }
-        }
+        },
     );
 };
