1c05cdd2778d9e099ebc8df8aee2ce8be5d76e5e
mrshappy0 <ajs11300@gmail.com>
2025-03-13 15:13:53 -0600

    add linting

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,21 +1,18 @@
-import { defineConfig } from "wxt";
+import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-    extensionApi: "chrome",
-    modules: ["@wxt-dev/module-react"],
+    extensionApi: 'chrome',
+    modules: ['@wxt-dev/module-react'],
     manifest: {
-        permissions: [
-            "storage",
-            "activeTab",
-            "contextMenus",
-            "scripting",
-        ],
-        description: "Scrape recipes and save them to a Mealie instance.",
-        name: "Mini Mealie",
+        permissions: ['storage', 'activeTab', 'contextMenus', 'scripting'],
+        description: 'Scrape recipes and save them to a Mealie instance.',
+        name: 'Mini Mealie',
     },
     // https://wxt.dev/guide/essentials/config/auto-imports.html
-    // imports: {
-    //     // ...
-    //   },
+    imports: {
+        eslintrc: {
+            enabled: 9,
+        },
+    },
 });
