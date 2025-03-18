c379fc10837263f09703062d3313c4ad599c34ad
mrshappy0 <ajs11300@gmail.com>
2025-03-18 17:49:31 -0600

    add @eslint/css and sort imports linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,32 @@
+import css from '@eslint/css';
 import pluginJs from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import pluginReact from 'eslint-plugin-react';
 import pluginSecurity from 'eslint-plugin-security';
+import importSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+
 import autoImports from './.wxt/eslint-auto-imports.mjs';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    // { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx,css,html}"], // TODO: add css once @eslint/css 0.5.0 is
+    { ...pluginJs.configs.recommended, files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    ...tseslint.configs.recommended.map((config) => ({
+        ...config,
+        files: ['**/*.{js,ts,jsx,tsx}'],
+    })),
+    { ...pluginReact.configs.flat.recommended, files: ['**/*.{js,ts,jsx,tsx}'] },
+    {
+        files: ['**/*.{js,ts,jsx,tsx}'],
+        plugins: {
+            'simple-import-sort': importSort,
+        },
+        rules: {
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
+        },
+    },
     {
         files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
         languageOptions: { globals: globals.browser },
@@ -18,18 +36,24 @@ export default [
                 'jsx-runtime': 'automatic',
             },
         },
-    },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    {
         rules: {
             'react/react-in-jsx-scope': 'off',
             'react/jsx-no-undef': ['error', { allowGlobals: true }],
         },
     },
-    pluginSecurity.configs.recommended,
-    eslintPluginPrettierRecommended,
+    {
+        files: ['**/*.css'],
+        language: 'css/css',
+        plugins: {
+            css,
+        },
+        rules: {
+            ...css.configs.recommended.rules,
+            'css/require-baseline': 'off',
+        },
+    },
+    { ...pluginSecurity.configs.recommended, files: ['**/*.{js,ts,jsx,tsx}'] },
+    { ...eslintPluginPrettierRecommended, files: ['**/*.{js,ts,jsx,tsx}'] },
     autoImports,
     {
         ignores: ['node_modules', '.wxt', '.output', 'coverage', 'html', 'coverage-report'],
