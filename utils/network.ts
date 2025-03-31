44ac6a1f1117955924c710c022cfdb16f520c5db
Spencer <spencer.adler2@gmail.com>
2025-03-31 17:07:19 -0400

    Added Verify Connection

--- utils/network.ts
+++ utils/network.ts
@@ -48,3 +48,59 @@ export const scrapeRecipe = (url: string, tabId: number) => {
         },
     );
 };
+
+export const verifyConnection = (url: string, tabId: number) => {
+    chrome.storage.sync.get(
+        ['mealieServer', 'mealieApiToken'],
+        ({ mealieServer, mealieApiToken }) => {
+            if (!mealieServer) {
+                showBadge('❌', 4);
+                return;
+            }
+
+            if (!mealieApiToken) {
+                showBadge('❌', 4);
+                return;
+            }
+            chrome.scripting.executeScript(
+                {
+                    target: { tabId },
+                    func: async (url, token) => {
+                        try {
+                            const verificationResponse = await fetch(`${url}/api/users/self`, {
+                                headers: {
+                                    Authorization: `Bearer ${token}`,
+                                    'Content-Type': 'application/json',
+                                },
+                            });
+                            if (!verificationResponse.ok) {
+                                throw new Error(
+                                    `Verification failed! status: ${verificationResponse.status}`,
+                                );
+                            }
+                            const verificationData = await verificationResponse.json();
+                            const username = verificationData.username;
+
+                            return username;
+                            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                        } catch (error) {
+                            //Do we want the username to be "Not signed in" or something like that?
+                            return { username: null };
+                        }
+                    },
+                    args: [url, mealieApiToken],
+                },
+                (result) => {
+                    const username = result[0].result;
+                    // Not sure the issue for below
+                    if (username) {
+                        showBadge(`✅ ${username}`, 4);
+                    } else {
+                        // Should below have an username not signed in or a additional option for x plus not signed in.
+                        showBadge('❌', 4);
+                    }
+                },
+            );
+        },
+    );
+};
