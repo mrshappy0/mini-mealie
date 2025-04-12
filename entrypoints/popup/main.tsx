560745aefcdc3e8b2ac510d39d68604df29494b1
mrshappy0 <ajs11300@gmail.com>
2025-04-12 13:30:31 -0600

    remove broken google analytics for now

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -5,35 +5,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
-function addGoogleAnalytics() {
-    if (!document.getElementById('gtag-script')) {
-        const script = document.createElement('script');
-        script.id = 'gtag-script';
-        script.async = true;
-        script.src = 'https://www.googletagmanager.com/gtag/js?id=G-59Y1CN5WTK';
-        document.head.appendChild(script);
-
-        const inlineScript = document.createElement('script');
-        inlineScript.textContent = `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-59Y1CN5WTK');
-        `;
-        document.head.appendChild(inlineScript);
-    }
-}
-
-const RootApp = () => {
-    useEffect(() => {
-        addGoogleAnalytics();
-    }, []);
-
-    return <App />;
-};
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <RootApp />
+        <App />
     </StrictMode>,
 );
